4c1959ab3a21bb8126bce76ef054cf7e72ae8de3
homingricky <homingricky@gmail.com>
2022-09-24 18:22:17 +0800

    intial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test-go-package
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import demogopackage "github.com/homingricky/demo-go-package"
+
+func main() {
+	demogopackage.FirstPrint("Hello World")
+}
